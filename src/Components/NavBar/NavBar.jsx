8e93444963075d7fd2a35604aaa3e6ebbc42f4c8
Jeffrey Peplinski <jeffreypeplinski@jeffreys-mbp.lan>
2022-02-09 12:46:18 -0500

    Deleted unnecessary files, created new file structure, and added NavBar component.

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+
+const NavBar = (props) => {
+    return ( 
+        <div>
+            <nav>
+                <h2>SocialFeed</h2>
+            </nav>
+        </div>
+     );
+}
+ 
+export default NavBar;
